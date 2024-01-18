99cf381145fc96b926e55ceb1b9c0a443089aa19
Paola Perán <151035716+pperval015@users.noreply.github.com>
2024-01-18 14:25:39 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function pedirdatos() {
-    let nombre, edad;
-        nombre = prompt("¿Cómo te llamas?");
-        edad = prompt("¿Cuántos años tienes?");
-        alert("Hola " + nombre + ", veo que tienes " + edad + " años.");
-}
