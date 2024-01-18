262226bf1adb323e3f8b7f18cd279e6ca87d577b
Paola Perán <151035716+pperval015@users.noreply.github.com>
2024-01-18 14:21:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function pedirdatos() {
+    let nombre, edad;
+        nombre = prompt("¿Cómo te llamas?");
+        edad = prompt("¿Cuántos años tienes?");
+        alert("Hola " + nombre + ", veo que tienes " + edad + " años.");
+}
