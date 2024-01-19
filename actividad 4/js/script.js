26ac04db7d7cc1aa4bc2cd989f727a808aa4330f
Paola Perán <151035716+pperval015@users.noreply.github.com>
2024-01-19 11:49:27 +0100

    Add files via upload

---
+++ actividad 4/js/script.js
@@ -0,0 +1,11 @@
+function Calcular() {
+    let a, b, c, d, e, f, g;
+        a = parseInt(prompt ("Inserta el primer número: "));
+        b = parseInt(prompt ("Inserta el segundo número: "));
+        c = a+b;
+        d = a-b;
+        e = a*b;
+        f = a/b;
+        g = a%b;
+    alert ("El primer número es: " + a + "\nEl primer número es: " + b + "\n La suma es: " + c + "\n La resta es: " + d + "\n El producto es: " + e + "\n La división es: " + f + "\n El resto es: " + g)
+    }
