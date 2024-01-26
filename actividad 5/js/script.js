988924c77264e01f77600ceb3d5e753682ab6112
Paola Perán <151035716+pperval015@users.noreply.github.com>
2024-01-26 11:11:29 +0100

    Add files via upload

---
+++ actividad 5/js/script.js
@@ -0,0 +1,7 @@
+function Calcular() {
+    let numero1;
+    let numero2;
+    numero1 = parseInt(document .getElementById("numero1").value);
+    numero2 = parseInt(document.getElementById("numero2").value);
+    document.getElementById("solución").innerHTML= "El primer número es " + numero1 + "<br>El segundo número es " + numero2 + "<br>La suma es " + (numero1+numero2) + "<br>La resta es " +(numero1-numero2) + "<br>El producto es " + (numero1*numero2) +  "<br>La división es " + (numero1/numero2) + "<br>El resto es " + (numero1%numero2)
+}
