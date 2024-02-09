15fdf7a88ffa7138c422558fb804b99230b886c5
Paola Perán <151035716+pperval015@users.noreply.github.com>
2024-02-09 11:58:34 +0100

    Add files via upload

---
+++ actividad 9/js/script.js
@@ -0,0 +1,11 @@
+function Factorial() {
+    let numero = parseInt(document.getElementById("numero").value, 10);
+    let resultado= 1;
+    let iteracion= 1;
+
+    while (iteracion <= numero) {
+        resultado = resultado * iteracion;
+        iteracion= iteracion + 1;
+        document.getElementById("solucion").innerHTML = "El factorial de " + numero + " es " + resultado;
+        }
+}
